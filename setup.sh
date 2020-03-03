c1b5318b48f4b20df7c70e9897828df42a6e0335
Kotaro Abe <abekoh.bcky@gmail.com>
2020-03-04 01:05:55 +0900

    update

--- setup.sh
+++ setup.sh
@@ -5,5 +5,11 @@ cd webflux-todo
 mvn install -DskipTests
 cd ../
 
+# npm install
+cd workspace-front
+npm install
+npm run build:compose
+cd ../
+
 # copy initial sql
 cp webflux-todo/src/main/resources/schema.sql mysql_init/
