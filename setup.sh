0a3f469edad992d64be3a76abe138f251bf36d29
Kotaro Abe <abekoh.bcky@gmail.com>
2020-03-16 01:26:41 +0900

    update setup

--- setup.sh
+++ setup.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
 # make jar
-cd webflux-todo
-mvn install -DskipTests
-cd ../
+docker run --rm -v $PWD/webflux-todo:/app -v $HOME/.m2/repository:/root/.m2/repository -w /app maven:3-jdk-11 mvn install -DskipTests
 
 # npm install
-cd workspace-front
-npm install
-npm run build:compose
-cd ../
+docker run --rm -v $PWD/workspace-front:/app -v $HOME/.npm:/root/.npm -w /app node:12.14.0 /bin/bash -c "yarn install ; yarn build:compose"
 
 # copy initial sql
 cp webflux-todo/src/main/resources/schema.sql mysql_init/
